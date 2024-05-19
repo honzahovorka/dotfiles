abd803f6b96d343e46e355295daa2f2db9892b6c
Honza Hovorka <honza.hovorka@gmail.com>
2024-05-19 20:26:56 +0200

    feat(nvim): clean up lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,210 +1,161 @@
 return {
-  'folke/neodev.nvim',
-  {
-    'dmmulroy/ts-error-translator.nvim',
-    opts = {},
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    { 'williamboman/mason.nvim',           config = true }, -- NOTE: Must be loaded before dependants
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    {
+      'j-hui/fidget.nvim',
+      config = function()
+        require('fidget').setup({
+          notification = {
+            window = {
+              winblend = 0,
+            },
+          }
+        })
+      end
+    },
+    { 'folke/neodev.nvim',                 opts = {} },
+    { 'dmmulroy/ts-error-translator.nvim', opts = {} },
   },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require('neodev').setup({})
-      local nvim_lsp = require('lspconfig')
-      local protocol = require('vim.lsp.protocol')
-
-      require('mason').setup()
-
-      local lsp_servers = {
-        'tsserver',
-        'lua_ls',
-        'gopls',
-        'vimls',
-        'yamlls',
-        'terraformls',
-        'dockerls',
-        'jsonls',
-        'eslint',
-        'solargraph',
-        'zls',
-        'clangd',
-      }
-
-      require('mason-lspconfig').setup({
-        ensure_installed = lsp_servers,
-      })
-
-      vim.cmd [[autocmd ColorScheme * hi! link FloatBorder Normal]]
-      vim.cmd [[autocmd ColorScheme * hi! link NormalFloat Normal]]
-
-      -- draw a border around the hover and signature help boxes
-      local border = {
-        { "╭", 'FloatBorder' },
-        { "─", 'FloatBorder' },
-        { "╮", 'FloatBorder' },
-        { "│", 'FloatBorder' },
-        { "╯", 'FloatBorder' },
-        { "─", 'FloatBorder' },
-        { "╰", 'FloatBorder' },
-        { "│", 'FloatBorder' },
-      }
-
-      local on_attach = function(client, bufnr)
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-        --Enable completion triggered by <c-x><c-o>
-        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        -- Mappings.
-        local opts = { noremap = true, silent = true }
-
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        buf_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', opts)
-        buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        buf_set_keymap('n', '<space>h', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
-        buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-        buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-        buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setqflist()<CR>', opts)
-        buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-        buf_set_keymap('n', '<space>sd', '<cmd>lua require("telescope.builtin").diagnostics()<CR>', opts)
-
-        -- formatting
-        if client.name == 'tsserver' then
-          local ts_utils = require('nvim-lsp-ts-utils')
-          ts_utils.setup({})
-          ts_utils.setup_client(client)
+  config = function()
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('custom-lsp-attach', { clear = true }),
+      callback = function(event)
+        local map = function(keys, func)
+          vim.keymap.set('n', keys, func, { buffer = event.buf })
         end
 
-        --protocol.SymbolKind = { }
-        protocol.CompletionItemKind = {
-          '', -- Text
-          '', -- Method
-          '', -- Function
-          '', -- Constructor
-          '', -- Field
-          '', -- Variable
-          '', -- Class
-          'ﰮ', -- Interface
-          '', -- Module
-          '', -- Property
-          '', -- Unit
-          '', -- Value
-          '', -- Enum
-          '', -- Keyword
-          '﬌', -- Snippet
-          '', -- Color
-          '', -- File
-          '', -- Reference
-          '', -- Folder
-          '', -- EnumMember
-          '', -- Constant
-          '', -- Struct
-          '', -- Event
-          'ﬦ', -- Operator
-          '', -- TypeParameter
+        map('gd', require('telescope.builtin').lsp_definitions)
+        map('gr', require('telescope.builtin').lsp_references)
+        map('gI', require('telescope.builtin').lsp_implementations)
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions)
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols)
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+        map('<leader>rn', vim.lsp.buf.rename)
+        map('<leader>ca', vim.lsp.buf.code_action)
+        map('K', vim.lsp.buf.hover)
+        map('gD', vim.lsp.buf.declaration)
+
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        if client and client.server_capabilities.documentHighlightProvider then
+          local highlight_augroup = vim.api.nvim_create_augroup('custom-lsp-highlight', { clear = false })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
+
+          vim.api.nvim_create_autocmd('LspDetach', {
+            group = vim.api.nvim_create_augroup('custom-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'custom-lsp-highlight', buffer = event2.buf }
+            end,
+          })
+        end
+
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+          end)
+        end
+
+        local border = {
+          { '╭', 'FloatBorder' },
+          { '─', 'FloatBorder' },
+          { '╮', 'FloatBorder' },
+          { '│', 'FloatBorder' },
+          { '╯', 'FloatBorder' },
+          { '─', 'FloatBorder' },
+          { '╰', 'FloatBorder' },
+          { '│', 'FloatBorder' },
         }
 
         vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
         vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
-      end
+      end,
+    })
 
-      vim.diagnostic.config({
-        underline = true,
-        float = {
-          show_header = true,
-          border = border,
-        },
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-        severity_sort = true,
-        -- delay update diagnostics
-        update_in_insert = false,
-      })
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-      nvim_lsp.tsserver.setup({
+    local servers = {
+      tsserver = {
         init_options = {
           preferences = {
             importModuleSpecifierPreference = 'relative'
           }
         },
-        on_attach = on_attach,
         filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
-        capabilities = capabilities,
-      })
-
-      nvim_lsp.gopls.setup({
-        on_attach = on_attach,
-        filetypes = { 'go', 'gomod' },
-        cmd = { 'gopls', '--remote=auto' },
-        rootPatterns = { 'go.mod', '.git' },
-        capabilities = capabilities,
-      })
-
-      nvim_lsp.rust_analyzer.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      nvim_lsp.solargraph.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        cmd = { os.getenv('HOME') .. '/.rbenv/shims/solargraph', 'stdio' },
-      })
-
-      nvim_lsp.lua_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
         settings = {
-          Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            diagnostics = { disable = { 'missing-fields' } },
+          javascript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = false,
+            },
           },
-        },
-      })
-
-      nvim_lsp.eslint.setup({
-        root_dir = require('lspconfig.util').root_pattern(
-          '.eslintrc.js',
-          'node_modules',
-          '.git'
-        ),
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      nvim_lsp.zls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      nvim_lsp.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end,
-  },
-  {
-    'j-hui/fidget.nvim',
-    config = function()
-      require('fidget').setup({
-        notification = {
-          window = {
-            winblend = 0,
+          typescript = {
+            inlayHints = {
+              includeInlayEnumMemberValueHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayVariableTypeHints = false,
+            },
           },
         }
-      })
-    end
-  },
-  'williamboman/mason.nvim',
-  'williamboman/mason-lspconfig.nvim',
-  'jose-elias-alvarez/nvim-lsp-ts-utils',
+      },
+
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnippet = 'Replace',
+            },
+            diagnostics = {
+              globals = { 'vim' },
+            },
+
+            hint = {
+              enable = true,
+            },
+          },
+        },
+      },
+    }
+
+    require('mason').setup()
+
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      'stylua', -- Used to format Lua code
+    })
+
+    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      }
+    })
+  end
 }
