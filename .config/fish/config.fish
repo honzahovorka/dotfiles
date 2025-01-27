205119820f61fbf2512321dd2b1bfe63d237dc05
Honza Hovorka <honza.hovorka@gmail.com>
2025-01-27 13:20:36 +0100

    feat(yazi): add yazi config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,7 @@ set -gx OMF_CONFIG $HOME/.config/omf
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore-vcs --hidden'
 set -Ux FZF_DEFAULT_OPTS '--height=90% --layout=reverse --info=inline --border --margin=1 --padding=1 --color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 --color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf --color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284'
 
-# Autojump
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+set -x EDITOR 'nvim'
 
 # Node.js
 export N_PREFIX=$HOME/.n
