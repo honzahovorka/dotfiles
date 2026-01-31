3f6d4f0859b6b850bb74a3c78946df35e54fc54e
Honza Hovorka <honza.hovorka@gmail.com>
2026-01-31 11:07:00 +0100

    fix(nvim): require utils.lua on init

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,46 +1,47 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('options')
-require('keymaps')
-require('lsp-config')
-require('autocmds')
+require("options")
+require("utils")
+require("keymaps")
+require("lsp-config")
+require("autocmds")
 
-require('lazy').setup('plugins', {
-  ui = {
-    border = 'single',
-    icons = {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-    },
-  },
-  install = {
-    colorscheme = { 'catppuccin' },
-  },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
+require("lazy").setup("plugins", {
+	ui = {
+		border = "single",
+		icons = {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+		},
+	},
+	install = {
+		colorscheme = { "catppuccin" },
+	},
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
 })
