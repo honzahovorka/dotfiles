b445cb0331b9230ce3dafe8d403b1e7260022de3
Honza Hovorka <honza.hovorka@gmail.com>
2026-05-11 14:10:58 +0200

    fix(nvim): treesitter config for nvim 0.12+

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
 		lazy = "false",
 		build = ":TSUpdate",
 	},
-	"nvim-treesitter/playground",
-	"nvim-treesitter/nvim-treesitter-textobjects",
 }
