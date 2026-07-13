dadb09031501fe7361e7297249d6a1c6bcfceb42
Honza Hovorka <honza.hovorka@gmail.com>
2026-07-13 11:11:24 +0200

    feat(nvim): neogit is back

--- common/.config/nvim/lua/plugins/git.lua
+++ common/.config/nvim/lua/plugins/git.lua
@@ -1,40 +1,59 @@
 return {
-  {
-    'tpope/vim-fugitive',
-    cmd = { 'G', 'Git', 'Gdiffsplit', 'Gread', 'Gwrite', 'Ggrep', 'GMove', 'GDelete', 'GBrowse', 'GRemove', 'GRename', 'Glgrep', 'Gedit' },
-    ft = { 'fugitive' },
-    keys = {
-      { '<leader>gg', '<cmd>G<cr>', desc = 'Fugitive status' },
-    },
-  },
-  {
-    'sindrets/diffview.nvim',
-    opts = {
-      view = {
-        merge_tool = {
-          layout = 'diff3_mixed',
-        },
-      },
-    }
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('gitsigns').setup()
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			"Gdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+			"GRemove",
+			"GRename",
+			"Glgrep",
+			"Gedit",
+		},
+		ft = { "fugitive" },
+		keys = {
+			{ "<leader>gg", "<cmd>G<cr>", desc = "Fugitive status" },
+		},
+	},
+	{
+		"sindrets/diffview.nvim",
+		opts = {
+			view = {
+				merge_tool = {
+					layout = "diff3_mixed",
+				},
+			},
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup()
 
-      vim.keymap.set('n', '<leader>gs', ':Gitsigns<CR>', { silent = true })
-    end
-  },
-  {
-    'NeogitOrg/neogit',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('neogit').setup({
-        integrations = {
-          diffview = true,
-        }
-      })
-    end
-  },
+			vim.keymap.set("n", "<leader>gs", ":Gitsigns<CR>", { silent = true })
+		end,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			-- Route GPG pinentry to a GUI popup (pinentry-qt) for Neogit's async
+			-- commits; terminal git keeps the inline pinentry-tty. See ~/.gnupg/pinentry-switch.
+			vim.env.PINENTRY_USER_DATA = "gui"
+			require("neogit").setup({
+				integrations = {
+					diffview = true,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { silent = true })
+		end,
+	},
 }
